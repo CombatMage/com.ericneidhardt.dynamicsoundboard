c4c7026f3a61448b2518f4403698e4f63edcf70d
Eric Neidhardt <eric.neidhardt@sevenval.com>
2014-09-23 10:33:10 +0200

    src: started action mode

--- app/src/main/java/com/ericneidhardt/dynamicsoundboard/customview/NavigationDrawerListL.java
+++ app/src/main/java/com/ericneidhardt/dynamicsoundboard/customview/NavigationDrawerListL.java
@@ -2,13 +2,17 @@ package com.ericneidhardt.dynamicsoundboard.customview;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
 import com.ericneidhardt.dynamicsoundboard.NavigationDrawerFragment;
 
 
-public abstract class NavigationDrawerListL extends FrameLayout
+public abstract class NavigationDrawerListL extends FrameLayout implements ActionMode.Callback
 {
 	protected NavigationDrawerFragment parent;
+	protected ActionMode actionMode;
 
 	public NavigationDrawerListL(Context context) {
 		super(context);
@@ -27,7 +31,28 @@ public abstract class NavigationDrawerListL extends FrameLayout
 		if (this.parent == null)
 			throw new NullPointerException("Cannot prepare deletion, because the containing fragment is null");
 
+		this.actionMode = this.parent.getActivity().startActionMode(this);
+	}
 
+	@Override
+	public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+		// TODO inflate stuff
 
+		return true;
+	}
+
+	@Override
+	public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+		return false;
+	}
+
+	@Override
+	public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+		return false;
+	}
+
+	@Override
+	public void onDestroyActionMode(ActionMode mode) {
+		this.actionMode = null;
 	}
 }
