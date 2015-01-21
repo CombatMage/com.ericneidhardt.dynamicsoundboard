990ea7550593b0c3590245901a8b3b49cf716e89
unknown <eric.neidhardt@PC-7153AE.int.sevenval.com>
2015-01-21 09:55:11 +0100

    implemented backhanding

--- app/src/main/java/com/ericneidhardt/dynamicsoundboard/preferences/SettingsActivity.java
+++ app/src/main/java/com/ericneidhardt/dynamicsoundboard/preferences/SettingsActivity.java
@@ -1,20 +1,43 @@
 package com.ericneidhardt.dynamicsoundboard.preferences;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import com.ericneidhardt.dynamicsoundboard.R;
 
 /**
  * Created by eric.neidhardt on 21.01.2015.
  */
-public class SettingsActivity extends Activity
+public class SettingsActivity extends ActionBarActivity
 {
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		this.getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
+		this.setContentView(R.layout.activity_preferences);
+
+		this.createActionbar();
+
+		this.getFragmentManager().beginTransaction().replace(R.id.main_frame, new SettingsFragment()).commit();
+	}
+
+	private void createActionbar()
+	{
+		Toolbar toolbar = (Toolbar) this.findViewById(R.id.toolbar);
+		this.setSupportActionBar(toolbar);
+		this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		int id = item.getItemId();
+		if (id == android.R.id.home)
+			this.finish();
+
+		return super.onOptionsItemSelected(item);
 	}
 
 	public static class SettingsFragment extends PreferenceFragment
