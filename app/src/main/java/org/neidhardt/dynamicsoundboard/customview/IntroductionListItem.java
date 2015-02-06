8d021dd571760366271d2eac6bdc80da1527de9d
unknown <eric.neidhardt@PC-7153AE.int.sevenval.com>
2015-02-06 13:26:16 +0100

    added introduction list item

---
+++ app/src/main/java/org/neidhardt/dynamicsoundboard/customview/IntroductionListItem.java
@@ -0,0 +1,35 @@
+package org.neidhardt.dynamicsoundboard.customview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by eric.neidhardt on 06.02.2015.
+ */
+public class IntroductionListItem extends RelativeLayout
+{
+	public IntroductionListItem(Context context)
+	{
+		super(context);
+		this.init();
+	}
+
+	public IntroductionListItem(Context context, AttributeSet attrs)
+	{
+		super(context, attrs);
+		this.init();
+	}
+
+	public IntroductionListItem(Context context, AttributeSet attrs, int defStyleAttr)
+	{
+		super(context, attrs, defStyleAttr);
+		this.init();
+	}
+
+	private void init()
+	{
+		// TODO
+	}
+
+}
