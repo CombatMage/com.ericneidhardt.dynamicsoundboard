6af57618be8a685aae2353789989ccde447759bd
unknown <eric.neidhardt@PC-7153AE.int.sevenval.com>
2015-02-16 11:35:03 +0100

    removed warning and unused new release

--- app/src/main/java/org/neidhardt/dynamicsoundboard/customview/DividerItemDecoration.java
+++ app/src/main/java/org/neidhardt/dynamicsoundboard/customview/DividerItemDecoration.java
@@ -50,7 +50,7 @@ public class DividerItemDecoration extends RecyclerView.ItemDecoration
 	}
 
 	@Override
-	public void onDraw(Canvas c, RecyclerView parent)
+	public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state)
 	{
 		drawForVerticalList(c, parent);
 	}
