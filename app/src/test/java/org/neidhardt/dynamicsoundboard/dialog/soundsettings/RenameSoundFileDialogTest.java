62a3de5ff1891b516cc298c1f4cebb2136f6e1e6
unknown <eric.neidhardt@PC-7153AE.int.sevenval.com>
2015-04-14 07:52:05 +0200

    more testing

--- app/src/test/java/org/neidhardt/dynamicsoundboard/dialog/soundsettings/RenameSoundFileDialogTest.java
+++ app/src/test/java/org/neidhardt/dynamicsoundboard/dialog/soundsettings/RenameSoundFileDialogTest.java
@@ -3,8 +3,13 @@ package org.neidhardt.dynamicsoundboard.dialog.soundsettings;
 import org.junit.Test;
 import org.neidhardt.dynamicsoundboard.ActivityTest;
 import org.neidhardt.dynamicsoundboard.dao.MediaPlayerData;
+import org.neidhardt.dynamicsoundboard.mediaplayer.EnhancedMediaPlayer;
 import org.neidhardt.dynamicsoundboard.testutils.TestDataGenerator;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.*;
 
 /**
@@ -22,6 +27,21 @@ public class RenameSoundFileDialogTest extends ActivityTest
 		RenameSoundFileDialog dialog = (RenameSoundFileDialog) this.activity.getFragmentManager().findFragmentByTag(RenameSoundFileDialog.TAG);
 		assertNotNull(dialog);
 
-		//TODO write test
+		this.service.addNewSoundToSoundsAndDatabase(TestDataGenerator.getRandomPlayerData());
+		this.service.addNewSoundToSoundsAndDatabase(TestDataGenerator.getRandomPlayerData());
+
+		MediaPlayerData data2 = TestDataGenerator.getRandomPlayerData();
+		data2.setUri(data.getUri());
+		this.service.addNewSoundToPlaylistAndDatabase(data2);
+
+		MediaPlayerData data3 = TestDataGenerator.getRandomPlayerData();
+		data3.setUri(data.getUri());
+		this.service.addNewSoundToSoundsAndDatabase(data3);
+
+		List<EnhancedMediaPlayer> players = dialog.getPlayersWithMatchingUri(data.getUri());
+		assertThat(players.size(), equalTo(2));
+
+		assertTrue(players.get(0).getMediaPlayerData().getUri().equals(data2.getUri()) || players.get(1).getMediaPlayerData().getUri().equals(data2.getUri()));
+		assertTrue(players.get(0).getMediaPlayerData().getUri().equals(data3.getUri()) || players.get(1).getMediaPlayerData().getUri().equals(data3.getUri()));
 	}
 }
