a62be38ed09e4ab3fde2ec75d0f73d0bd7e48ef4
unknown <eric.neidhardt@PC-7153AE.int.sevenval.com>
2015-06-24 07:43:43 +0200

    misc

--- app/src/main/java/org/neidhardt/dynamicsoundboard/dao/SoundboardDaoOpenHelper.java
+++ app/src/main/java/org/neidhardt/dynamicsoundboard/dao/SoundboardDaoOpenHelper.java
@@ -2,13 +2,15 @@ package org.neidhardt.dynamicsoundboard.dao;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
+import org.neidhardt.dynamicsoundboard.misc.Logger;
 
 /**
- * Created by eric.neidhardt on 27.03.2015.
+ * File created by eric.neidhardt on 27.03.2015.
  */
 public class SoundboardDaoOpenHelper extends DaoMaster.OpenHelper
 {
+	private static final String TAG = SoundboardDaoOpenHelper.class.getName();
+
 	public SoundboardDaoOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory)
 	{
 		super(context, name, factory);
@@ -17,7 +19,7 @@ public class SoundboardDaoOpenHelper extends DaoMaster.OpenHelper
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
 	{
-		Log.i("greenDAO", "Upgrading schema from version " + oldVersion + " to " + newVersion + " by dropping all tables");
+		Logger.d(TAG, "Upgrading schema from version " + oldVersion + " to " + newVersion + " by dropping all tables");
 
 		if (oldVersion <= 9)
 			SoundLayoutDao.createTable(db, false);
