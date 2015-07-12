a27a914831a35bb332faa39cfb96e281931066d7
Eric Neidhardt <eric.neidhardt@sevenval.com>
2015-07-12 20:13:58 +0200

    wip

--- app/src/main/java/org/neidhardt/dynamicsoundboard/soundsheetmanagement/views/AddNewSoundSheetDialog.java
+++ app/src/main/java/org/neidhardt/dynamicsoundboard/soundsheetmanagement/views/AddNewSoundSheetDialog.java
@@ -7,9 +7,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatDialog;
 import android.view.View;
+import android.view.ViewGroup;
 import org.neidhardt.dynamicsoundboard.R;
 import org.neidhardt.dynamicsoundboard.dao.SoundSheet;
 import org.neidhardt.dynamicsoundboard.views.BaseDialog;
+import org.neidhardt.dynamicsoundboard.views.DialogBaseLayout;
 import org.neidhardt.dynamicsoundboard.views.edittext.CustomEditText;
 
 public class AddNewSoundSheetDialog extends BaseDialog implements View.OnClickListener
@@ -47,6 +49,9 @@ public class AddNewSoundSheetDialog extends BaseDialog implements View.OnClickLi
 	public Dialog onCreateDialog(Bundle savedInstanceState)
 	{
 		@SuppressLint("InflateParams") View view = this.getActivity().getLayoutInflater().inflate(R.layout.dialog_add_new_sound_sheet, null);
+
+		this.setMainView((DialogBaseLayout) view);
+
 		this.soundSheetName = (CustomEditText)view.findViewById(R.id.et_name_new_sound_sheet);
 		this.soundSheetName.setHint(this.suggestedName);
 
@@ -56,6 +61,8 @@ public class AddNewSoundSheetDialog extends BaseDialog implements View.OnClickLi
 		AppCompatDialog dialog = new AppCompatDialog(this.getActivity(), R.style.DialogTheme);
 		dialog.setContentView(view);
 		dialog.setTitle(R.string.dialog_add_new_sound_sheet_title);
+
+		view.getLayoutParams().height = ViewGroup.LayoutParams.WRAP_CONTENT;
 
 		return dialog;
 	}
