63784be1b2bba74290dfff83f560aa891c3deef0
Eric Neidhardt <eric.neidhardt@sevenval.com>
2015-11-12 17:20:26 +0100

    missing file

---
+++ app/src/main/kotlin/org/neidhardt/dynamicsoundboard/views/progressbar/ActivityProgressBar.kt
@@ -0,0 +1,95 @@
+package org.neidhardt.dynamicsoundboard.views.progressbar
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import de.greenrobot.event.EventBus
+import fr.castorflex.android.smoothprogressbar.SmoothProgressBar
+import org.neidhardt.dynamicsoundboard.misc.Logger
+import org.neidhardt.dynamicsoundboard.misc.longtermtask.events.LongTermTaskStateChangedEvent
+import org.neidhardt.dynamicsoundboard.views.presenter.ViewPresenter
+
+/**
+ * File created by eric.neidhardt on 22.05.2015.
+ */
+interface ActivityProgressBar
+{
+	fun setVisibility(v: Int)
+}
+
+class ActivityProgressBarView : SmoothProgressBar, ActivityProgressBar
+{
+	private var presenter: ActivityProgressBarPresenter = ActivityProgressBarPresenter(EventBus.getDefault())
+
+	constructor(context: Context) : super(context)
+
+	constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+	constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle)
+
+	override fun onFinishInflate()
+	{
+		super.onFinishInflate()
+		this.presenter.view = this
+	}
+
+	override fun onAttachedToWindow()
+	{
+		super.onAttachedToWindow()
+		this.presenter.onAttachedToWindow()
+	}
+
+	override fun onDetachedFromWindow()
+	{
+		this.presenter.onDetachedFromWindow()
+		super.onDetachedFromWindow()
+	}
+}
+
+class ActivityProgressBarPresenter
+(
+		override val eventBus: EventBus
+) : ViewPresenter<ActivityProgressBar?>
+{
+	private val TAG = javaClass.name
+
+	private var lastReceivedEvent: LongTermTaskStateChangedEvent? = null
+
+	override val isEventBusSubscriber: Boolean = true
+	override var view: ActivityProgressBar? = null
+		set(value)
+		{
+			field = value
+			if (this.lastReceivedEvent != null)
+				this.onEventMainThread(this.lastReceivedEvent as LongTermTaskStateChangedEvent)
+		}
+
+	/**
+	 * This is called by greenRobot EventBus in case a background task starts or finishes his execution
+	 * @param event delivered LongTermTaskStateChangedEvent
+	 */
+	@SuppressWarnings("unused")
+	fun onEventMainThread(event: LongTermTaskStateChangedEvent)
+	{
+		Logger.d(TAG, "onEvent() " + event)
+		this.lastReceivedEvent = event
+		val countOngoingTasks = event.nrOngoingTasks
+
+		if (countOngoingTasks > 0)
+			this.showProgressBar(true)
+		else
+			this.showProgressBar(false)
+	}
+
+	private fun showProgressBar(showProgressBar: Boolean)
+	{
+		Logger.d(TAG, "showProgressBar() " + showProgressBar)
+		val progressBar = this.view ?: return
+
+		if (showProgressBar)
+			progressBar.setVisibility(View.VISIBLE)
+		else
+			progressBar.setVisibility(View.GONE)
+	}
+
+}
