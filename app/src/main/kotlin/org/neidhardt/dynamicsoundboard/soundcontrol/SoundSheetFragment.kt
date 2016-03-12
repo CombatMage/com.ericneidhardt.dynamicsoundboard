28a5b1daa0858727df9b87af9f7fc8e1051a6c96
Eric Neidhardt <eric.neidhardt@gmail.com>
2016-03-12 15:05:54 +0100

    removed unused error handling

--- app/src/main/kotlin/org/neidhardt/dynamicsoundboard/soundcontrol/SoundSheetFragment.kt
+++ app/src/main/kotlin/org/neidhardt/dynamicsoundboard/soundcontrol/SoundSheetFragment.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.CoordinatorLayout
-import android.support.design.widget.Snackbar
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
@@ -21,10 +20,6 @@ import org.neidhardt.dynamicsoundboard.SoundboardApplication
 import org.neidhardt.dynamicsoundboard.dao.MediaPlayerData
 import org.neidhardt.dynamicsoundboard.dao.SoundSheet
 import org.neidhardt.dynamicsoundboard.fileexplorer.AddNewSoundFromDirectoryDialog
-import org.neidhardt.dynamicsoundboard.mediaplayer.MediaPlayerController
-import org.neidhardt.dynamicsoundboard.mediaplayer.PlayerAction
-import org.neidhardt.dynamicsoundboard.mediaplayer.events.MediaPlayerFailedEvent
-import org.neidhardt.dynamicsoundboard.mediaplayer.events.MediaPlayerFailedEventListener
 import org.neidhardt.dynamicsoundboard.misc.FileUtils
 import org.neidhardt.dynamicsoundboard.misc.IntentRequest
 import org.neidhardt.dynamicsoundboard.misc.Logger
@@ -62,8 +57,7 @@ class SoundSheetFragment :
 		DragSortRecycler.OnDragStateChangedListener,
 		DragSortRecycler.OnItemMovedListener,
 		OnOpenSoundDialogEventListener,
-		OnSoundsChangedEventListener,
-		MediaPlayerFailedEventListener
+		OnSoundsChangedEventListener
 {
 	private val LOG_TAG = javaClass.name
 
@@ -245,31 +239,6 @@ class SoundSheetFragment :
 	{
 		if (this.soundAdapter?.getValues()?.size == 0)
 			this.floatingActionButton?.show(true)
-	}
-
-	@Subscribe(threadMode = ThreadMode.MAIN)
-	override fun onEvent(event: MediaPlayerFailedEvent)
-	{
-		val snackbar = this.getSnackbarForError(event.player, event.failingAction)
-		snackbar.show()
-	}
-
-	private fun getSnackbarForError(failingPlayer: MediaPlayerController, failingAction: PlayerAction): Snackbar
-	{
-		val messageId = if (failingAction == PlayerAction.PLAY)
-			R.string.sound_control_play_failed
-		else
-			R.string.sound_control_failed
-
-		val message = this.resources.getString(messageId).replace("{%s0}", failingPlayer.mediaPlayerData.label)
-		val snackbar = Snackbar.make(this.coordinatorLayout as CoordinatorLayout, message, Snackbar.LENGTH_LONG)
-
-		snackbar.setAction(R.string.sound_control_reset_player, {
-			val soundUri = failingPlayer.mediaPlayerData.uri
-			failingPlayer.setSoundUri(soundUri)
-		})
-
-		return snackbar
 	}
 
 	override fun onEvent(event: SoundMovedEvent) {}
