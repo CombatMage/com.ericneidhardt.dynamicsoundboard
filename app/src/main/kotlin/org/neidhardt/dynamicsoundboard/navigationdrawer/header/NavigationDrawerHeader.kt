bbe0029a6133a3378542cb2261c1a0c1d0472cda
Eric Neidhardt <eric.neidhardt@sevenval.com>
2016-06-02 17:14:25 +0200

    update body text style

--- app/src/main/kotlin/org/neidhardt/dynamicsoundboard/navigationdrawer/header/NavigationDrawerHeader.kt
+++ app/src/main/kotlin/org/neidhardt/dynamicsoundboard/navigationdrawer/header/NavigationDrawerHeader.kt
@@ -153,6 +153,7 @@ class NavigationDrawerHeaderPresenter
 		if (this.view == null || this.soundLayoutModel == null)
 			return
 
+		this.openSoundLayouts = false
 		this.view?.animateLayoutChanges()
 		this.view?.showCurrentLayoutName(this.soundLayoutModel.getActiveSoundLayout().label)
 	}
