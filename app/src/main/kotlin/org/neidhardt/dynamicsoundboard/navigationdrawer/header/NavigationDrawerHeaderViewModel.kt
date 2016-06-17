8c37ae4fe2845562c15d67f2b723bdfa6447b976
Eric Neidhardt <eric.neidhardt@gmail.com>
2016-06-17 10:06:05 +0200

    first working example

--- app/src/main/kotlin/org/neidhardt/dynamicsoundboard/navigationdrawer/header/NavigationDrawerHeaderViewModel.kt
+++ app/src/main/kotlin/org/neidhardt/dynamicsoundboard/navigationdrawer/header/NavigationDrawerHeaderViewModel.kt
@@ -1,6 +1,8 @@
 package org.neidhardt.dynamicsoundboard.navigationdrawer.header
 
+import android.animation.ValueAnimator
 import android.databinding.BaseObservable
+import android.databinding.Bindable
 import android.view.View
 
 /**
@@ -10,9 +12,18 @@ class NavigationDrawerHeaderViewModel : BaseObservable() {
 
 	var title: String? = null
 
+	@Bindable
 	var indicatorRotation: Int = 0
+		set(value) {
+			field = value
+			this.notifyChange()
+		}
 
 	fun onChangeLayoutClicked(view: View) {
+		val animator = ValueAnimator.ofInt(0, 180)
+		animator.duration = 10000
+		animator.addUpdateListener { this.indicatorRotation = animator.animatedValue as Int }
+		animator.start()
 
 	}
 }
