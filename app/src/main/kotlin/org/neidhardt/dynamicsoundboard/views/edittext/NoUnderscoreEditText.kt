7ab4a031c53c76b676b2b10e73a5f9e1169c5a17
Eric Neidhardt <eric.neidhardt@sevenval.com>
2016-11-18 10:59:34 +0100

    remove unused update kotlin in build file

--- app/src/main/kotlin/org/neidhardt/dynamicsoundboard/views/edittext/NoUnderscoreEditText.kt
+++ app/src/main/kotlin/org/neidhardt/dynamicsoundboard/views/edittext/NoUnderscoreEditText.kt
@@ -7,7 +7,6 @@ import kotlinx.android.synthetic.main.view_no_underscore_edittext.view.*
 import org.neidhardt.dynamicsoundboard.R
 import org.neidhardt.ui_utils.views.CustomEditText
 import org.neidhardt.ui_utils.views.EditTextBackEvent
-import kotlin.properties.Delegates
 
 
 class NoUnderscoreEditText(context: Context, attrs: AttributeSet) : CustomEditText(context, attrs) {
