75b758804aa85a86675c20f7f991123ee38742ff
Eric Neidhardt <eric.neidhardt@sevenval.com>
2017-01-19 16:49:29 +0100

    removed unused added leak canary for debug build

--- app/src/debug/kotlin/org/neidhardt/dynamicsoundboard/DynamicSoundboardApplication.kt
+++ app/src/debug/kotlin/org/neidhardt/dynamicsoundboard/DynamicSoundboardApplication.kt
@@ -1,5 +1,6 @@
 package org.neidhardt.dynamicsoundboard;
 
+import com.squareup.leakcanary.LeakCanary
 import org.acra.ACRA
 import org.acra.annotation.ReportsCrashes
 import org.greenrobot.eventbus.EventBus
@@ -7,12 +8,22 @@ import org.greenrobot.eventbus.EventBus
 @ReportsCrashes(
 	mailTo = "eric.neidhardt@gmail.com"
 )
-class DynamicSoundboardApplication : SoundboardApplication()
-{
-	override fun onCreate()
-	{
+class DynamicSoundboardApplication : SoundboardApplication() {
+
+	init {
+		EventBus.builder().throwSubscriberException(BuildConfig.DEBUG).installDefaultEventBus()
+	}
+
+	override fun onCreate() {
 		super.onCreate()
-		//EventBus.builder().throwSubscriberException(BuildConfig.DEBUG).installDefaultEventBus()
+
+		if (LeakCanary.isInAnalyzerProcess(this)) {
+			// This process is dedicated to LeakCanary for heap analysis.
+			// You should not init your app in this process.
+			return;
+		}
+		LeakCanary.install(this);
+
 		ACRA.init(this)
 	}
 }
