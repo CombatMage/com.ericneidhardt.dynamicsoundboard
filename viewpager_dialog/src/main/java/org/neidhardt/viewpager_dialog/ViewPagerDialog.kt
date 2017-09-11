68d57e494c80e28cff5a80e3f73dffc2906208c0
Eric Neidhardt <eric.neidhardt@sevenval.com>
2017-09-11 09:25:09 +0200

    wip

--- viewpager_dialog/src/main/java/org/neidhardt/viewpager_dialog/ViewPagerDialog.kt
+++ viewpager_dialog/src/main/java/org/neidhardt/viewpager_dialog/ViewPagerDialog.kt
@@ -1,26 +1,49 @@
 package org.neidhardt.viewpager_dialog
 
-import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.support.v4.app.FragmentManager
+import org.neidhardt.viewpager_dialog.viewhelper.ViewPagerDialogBuilder
 
 /**
  * Created by eric.neidhardt@gmail.com on 08.09.2017.
  */
-class ViewPagerDialog : DialogFragment(), ViewPagerDialogContract.View<String> {
-
-	private val TAG = javaClass.name
+open class ViewPagerDialog : DialogFragment() {
 
 	companion object {
-		fun showDialog(fragmentManager: FragmentManager) {
-			// TODO
+
+		private val TAG = ViewPagerDialog::class.java.name
+		private val KEY_VIEW_DATA = "KEY_VIEW_DATA"
+
+		fun showDialog(fragmentManager: FragmentManager, viewData: Array<String>) {
+			val dialog = ViewPagerDialog()
+			dialog.arguments = Bundle().apply {
+				this.putStringArray(KEY_VIEW_DATA, viewData)
+			}
+			dialog.show(fragmentManager, TAG)
 		}
 	}
 
+	private lateinit var viewData: Array<String>
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		this.viewData = this.arguments?.getStringArray(KEY_VIEW_DATA) ?: emptyArray()
+	}
+
 	override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-		val dialogBuilder = AlertDialog.Builder(this.activity)
+		val dialogBuilder = ViewPagerDialogBuilder(this.activity)
+
+		this.getStringTitle()?.let { dialogBuilder.setTitle(it) }
+		this.getStringMessage()?.let { dialogBuilder.setMessage(it) }
+
+		dialogBuilder.setDataToDisplay(this.viewData)
+		dialogBuilder.setPositiveButton()
+
+		return dialogBuilder.create()
+
+		/*val dialogBuilder = AlertDialog.Builder(this.activity)
 		dialogBuilder.setTitle(R.string.request_permission_title)
 		dialogBuilder.setMessage(this.getMessageForPermissions())
 
@@ -28,15 +51,10 @@ class ViewPagerDialog : DialogFragment(), ViewPagerDialogContract.View<String> {
 			this.requestMissingPermissions()
 		})
 
-		return dialogBuilder.create()
+		return dialogBuilder.create()*/
 	}
 
-	override fun closeDialog() {
-		this.dismiss()
-	}
+	protected fun getStringTitle(): String? = null
 
-	override fun setDisplayedContent(viewData: Array<String>) {
-		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-	}
-
+	protected fun getStringMessage(): String? = null
 }
